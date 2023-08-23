9db5f95d4e972e310c614d3d9520cf0f6c2a05bb
FERNANDO RASPINI <fernandoraspini@outlook.com>
2023-08-23 17:04:05 -0400

    Primeiro commit

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+
+//		List<DadosTemporada> temporadas = new ArrayList<>();
+
+//		for(int i = 1; i<=dados.totalTemporadas(); i++) {
+//			json = consumoApi.obterDados ( "https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+//			DadosTemporada   dadosTemporada  = conversor.obterDados(json, DadosTemporada.class);
+//			temporadas.add(dadosTemporada);
+
+//		}
+//		temporadas.forEach(System.out::println);
+	}
+}
